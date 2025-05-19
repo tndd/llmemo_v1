96f499f06f889fdf1d75a9fd84fbdd693ce680a8
tau_m2 <36781951+tndd@users.noreply.github.com>
2025-05-20 04:23:42 +0900

    add todo

--- components/MainSidebarLayout.tsx
+++ components/MainSidebarLayout.tsx
@@ -5,6 +5,8 @@ interface MainSidebarLayoutProps {
   children?: React.ReactNode;
 }
 
+// TODO: home側sidebarの幅が変わってしまうのを解決する
+
 const MainSidebarLayout: React.FC<MainSidebarLayoutProps> = ({ children }) => {
   return (
     <div className="flex flex-col w-[256px] bg-[rgb(75,60,75)] text-white">
